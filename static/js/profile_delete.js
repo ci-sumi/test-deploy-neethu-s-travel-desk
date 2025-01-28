4362c112686ea1c2634c1f518adad35ceb882251
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-28 22:34:38 +0000

    Add function to open confirmation modal

---
+++ static/js/profile_delete.js
@@ -0,0 +1,9 @@
+// Function to open the confirmation modal
+function openModal() {
+    document.getElementById("deleteModal").style.display = "flex";
+  }
+
+  // Function to close the confirmation modal
+  function closeModal() {
+    document.getElementById("deleteModal").style.display = "none";
+  }
