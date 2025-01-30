57561e62ee77dc35aa1aa3953966be17c1532d59
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-30 22:47:06 +0000

    modify urls of accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('register',views.register,name='register'),
-    path('login',views.login,name='login'),
+    path('accounts/login',views.login,name='login'),
     path('logout',CustomLogoutView.as_view(),name='logout'),
     path('profile_view',views.profile_view,name='profile_view'),
-    path('update_profile',views.update_profile,name='update_profile'),
+    path('accounts/update_profile',views.update_profile,name='update_profile'),
     path('delete_profile',views.delete_profile,name='delete_profile'),
     path('contact',views.contact,name='contact'),
    
