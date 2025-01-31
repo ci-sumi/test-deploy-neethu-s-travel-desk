052258ebeb4a9e0ef5c7f2d30877fcdc374a6489
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-31 21:23:24 +0000

    Remove white spaces after validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,23 +1,19 @@
 from django.contrib import admin
 from django.urls import path
-from .import views
+from . import views
 from .views import CustomLogoutView
 from django.conf.urls import handler404
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',views.index,name='index'),
-    path('register',views.register,name='register'),
-    path('login',views.login,name='login'),
-    path('logout',CustomLogoutView.as_view(),name='logout'),
-    path('profile_view',views.profile_view,name='profile_view'),
-    path('update_profile',views.update_profile,name='update_profile'),
-    path('delete_profile',views.delete_profile,name='delete_profile'),
-    path('contact',views.contact,name='contact'),
-   
-    
-] 
-
-
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', CustomLogoutView.as_view(), name='logout'),
+    path('profile_view', views.profile_view, name='profile_view'),
+    path('update_profile', views.update_profile, name='update_profile'),
+    path('delete_profile', views.delete_profile, name='delete_profile'),
+    path('contact', views.contact, name='contact'),
+]
 
 handler404 = 'accounts.views.custom_404'
