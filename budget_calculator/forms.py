3d4314a8be50892696ae2181165a42a8e2920308
ci-sumi <tharayilsurendransumi@gmail.com>
2025-01-31 21:18:50 +0000

    Remove white spaces after validation

--- budget_calculator/forms.py
+++ budget_calculator/forms.py
@@ -2,24 +2,37 @@ from django import forms
 from .models import BudgetCalculator
 from accounts.models import Destination  # Assuming Destination model is in the accounts app
 
+
 class BudgetCalculatorForm(forms.ModelForm):
     class Meta:
         model = BudgetCalculator
         fields = [
-            'destination', 
-            'accommodation_cost', 
-            'transportation_cost', 
-            'food_cost', 
-            'activity_cost', 
-            'number_of_adults', 
-            'number_of_infants'
+            'destination',
+            'accommodation_cost',
+            'transportation_cost',
+            'food_cost',
+            'activity_cost',
+            'number_of_adults',
+            'number_of_infants',
         ]
         widgets = {
             'destination': forms.Select(attrs={'class': 'form-control'}),
-            'accommodation_cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '$0.00'}),
-            'transportation_cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '$0.00'}),
-            'food_cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '$0.00'}),
-            'activity_cost': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': '$0.00'}),
-            'number_of_adults': forms.NumberInput(attrs={'class': 'form-control'}),
-            'number_of_infants': forms.NumberInput(attrs={'class': 'form-control'}),
+            'accommodation_cost': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': '$0.00'}
+            ),
+            'transportation_cost': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': '$0.00'}
+            ),
+            'food_cost': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': '$0.00'}
+            ),
+            'activity_cost': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': '$0.00'}
+            ),
+            'number_of_adults': forms.NumberInput(
+                attrs={'class': 'form-control'}
+            ),
+            'number_of_infants': forms.NumberInput(
+                attrs={'class': 'form-control'}
+            ),
         }
